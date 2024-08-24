cac0e4d70ba1207f030c8e03707472966e4d6adf
zetampo <zbanjar9@gmail.com>
2024-08-24 14:54:12 +0700

    add feature randomize color after each hover

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
     const grid = document.createElement("div");
-    grid.style.width = "30px";
-    grid.style.height = "30px";
+    grid.style.width = "32px";
+    grid.style.height = "32px";
     grid.style.border = "1px solid black";
     grid.style.boxSizing = "border-box";
     grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "blue";
+        grid.style.backgroundColor = generateNewColor();
     });
     container.appendChild(grid);
 }
@@ -17,7 +17,7 @@ button.addEventListener("click", () => {
     let numGrid = prompt("Enter the number of squares per side (max 100): ");
     numGrid = parseInt(numGrid);
     container.innerHTML = "";
-    let squareSize = 480 / numGrid;
+    let squareSize = 520 / numGrid;
     for (let i = 0; i < (numGrid * numGrid); i++) {
         const grid = document.createElement("div");
         grid.style.width = `${squareSize}px`;
@@ -25,9 +25,29 @@ button.addEventListener("click", () => {
         grid.style.border = "1px solid black";
         grid.style.boxSizing = "border-box";
         grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = "blue";
+            grid.style.backgroundColor = generateNewColor();
         });
         container.appendChild(grid);
     }
 });
+
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+
+
+function getCharacter(index) {
+    return hexCharacters[index];
+}
+
+function generateNewColor() {
+    let hexColorRep = "#";
+
+    for (let index = 0; index < 6; index++){
+        const randomPosition = Math.floor ( Math.random() * hexCharacters.length );
+        hexColorRep += getCharacter( randomPosition );
+    }
+
+    return hexColorRep;
+}
+
 
